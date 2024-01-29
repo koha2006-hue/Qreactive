cab8c7a2983b03973a3ddafbf2a352cd924316d2
Võ Minh Kiệt <kitv16@gmail.com>
2024-01-29 19:45:51 +0700

    mobile nav
    làm tạm

--- qreactive-banhang/src/components/MobileNav.tsx
+++ qreactive-banhang/src/components/MobileNav.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { PRODUCT_CATEGORIES } from '@/config'
+import { NAV_LINKS } from '@/config'
 import { Menu, X } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -53,7 +53,7 @@ const MobileNav = () => {
           <div className='relative flex w-full max-w-sm flex-col overflow-y-auto bg-white pb-12 shadow-xl'>
             <div className='flex px-4 pb-2 pt-5'>
               <button
-                type='button'
+                type='button' 
                 onClick={() => setIsOpen(false)}
                 className='relative -m-2 inline-flex items-center justify-center rounded-md p-2 text-gray-400'>
                 <X className='h-6 w-6' aria-hidden='true' />
@@ -62,40 +62,21 @@ const MobileNav = () => {
 
             <div className='mt-2'>
               <ul>
-                {PRODUCT_CATEGORIES.map((category) => (
-                  <li
-                    key={category.label}
+                {NAV_LINKS.map((link) => (
+                  <Link
+                    href={link.href}
+                    key={link.label}
                     className='space-y-10 px-4 pb-8 pt-10'>
                     <div className='border-b border-gray-200'>
                       <div className='-mb-px flex'>
                         <p className='border-transparent text-gray-900 flex-1 whitespace-nowrap border-b-2 py-4 text-base font-medium'>
-                          {category.label}
+                          {link.label}
                         </p>
                       </div>
                     </div>
 
-                    <div className='grid grid-cols-2 gap-y-10 gap-x-4'>
-                      {category.featured.map((item) => (
-                        <div
-                          key={item.name}
-                          className='group relative text-sm'>
-                          <div className='relative aspect-square overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75'>
-                            <Image
-                              fill
-                              src={item.imageSrc}
-                              alt='product category image'
-                              className='object-cover object-center'
-                            />
-                          </div>
-                          <Link
-                            href={item.href}
-                            className='mt-6 block font-medium text-gray-900'>
-                            {item.name}
-                          </Link>
-                        </div>
-                      ))}
-                    </div>
-                  </li>
+                    
+                  </Link>
                 ))}
               </ul>
             </div>
--- qreactive-banhang/src/config/index.ts
+++ qreactive-banhang/src/config/index.ts
@@ -1,26 +1,5 @@
 export const PRODUCT_CATEGORIES = [
   {
-    label: 'UI Kits',
-    value: 'ui_kits' as const,
-    featured: [
-      {
-        name: 'Editor picks',
-        href: `/products?category=ui_kits`,
-        imageSrc: '/nav/ui-kits/mixed.jpg',
-      },
-      {
-        name: 'New Arrivals',
-        href: '/products?category=ui_kits&sort=desc',
-        imageSrc: '/nav/ui-kits/blue.jpg',
-      },
-      {
-        name: 'Bestsellers',
-        href: '/products?category=ui_kits',
-        imageSrc: '/nav/ui-kits/purple.jpg',
-      },
-    ],
-  },
-  {
     label: 'Icons',
     value: 'icons' as const,
     featured: [
